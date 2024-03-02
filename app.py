f3165e911795b1ff4ab2771da28a30cf5d3c5eab
Thanawit Suwannikom <thanawit@Thanawits-MacBook-Pro-2.local>
2024-03-02 15:38:36 +0700

    add new page

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from datetime import datetime
+from flask_session import Session
+
 
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key_here'
+# SESSION_TYPE = "memcached"
+app.secret_key = 'intsightedu'
+sess = Session()
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sat_simulation_test_user:b0vlxEmUfEgoQ0jtrFdCtmTaZU5rE8Kl@dpg-cn822qq1hbls73d8aq10-a.singapore-postgres.render.com/sat_simulation_test'
 db = SQLAlchemy(app)
+# Session(app)
+
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 class UserLogin(db.Model):
     __tablename__ = 'user_login'
@@ -40,6 +49,17 @@ class Question(db.Model):
     answer_d = db.Column(db.String(1000), nullable=True)
     category = db.Column(db.String(100), nullable=False)
 
+class Answer(db.Model):
+    __tablename__ = 'sat_answers'
+    answer_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    question_id = db.Column(db.Integer, nullable=False)
+    answer = db.Column(db.String(1000), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.now)
+
+    def __repr__(self):
+        return f"<Answer(answer_id={self.answer_id}, username='{self.username}', question_id={self.question_id}, answer='{self.answer}', timestamp='{self.timestamp}')>"
+
 # Dummy user data (replace with a database in a real application)
 users = {'admin@gmail.com': '1234'}
 
@@ -59,12 +79,22 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        # test_key = request.form['test_key']
         user = UserLogin.query.filter_by(username=username, password=password).first()
-        if user:
+
+        if not user:
+            flash('Please contact admin to support.')
+            return render_template('login.html')
+            
+        if (user.is_authenticated) :
             login_user(user)
-            return redirect(url_for('exam'))
+            session["user"] = username
+            session['logged_in'] = True
+            session["key"] = 'intsight_edu'
+            # return redirect(url_for('test_info'))
+            return render_template('test-info.html', username=current_user)
         else:
-            flash('Invalid username or password')
+            flash('Please contact admin to support.')
     return render_template('login.html')
 
 # @app.route('/login', methods=['GET', 'POST'])
@@ -78,26 +108,48 @@ def login():
 #             return 'Invalid username or password'
 #     return render_template('login.html')
 
+@app.route('/test-info')
+@login_required
+def test_info():
+    return render_template('test-info.html', username=current_user)
+
 @app.route('/exam', methods=['GET', 'POST'])
 @login_required
 def exam():
     questions = Question.query.order_by(Question.question_id).first()
-    return render_template('exam.html', current_question=questions)
+    remaining_seconds = 32 * 60
+    return render_template('exam.html', current_question=questions, remaining_time=remaining_seconds)
 
-@app.route('/next', methods=['POST'])
+@app.route('/next', methods=['GET', 'POST'])
 def next_question():
     question_id = int(request.form['question_id'])
     questions = Question.query.get_or_404(question_id)
+    if questions.question_id in [1, 11, 21, 31]:
+        remaining_seconds = 32*60
+    else:
+        data = request.json
+        # minutes = data.get('minutes')
+        # seconds = data.get('seconds')
+        # remaining_seconds = data.get('remaining_time')
+        remaining_seconds = redirect('/update-timer')
     # next_question = Question.query.filter(Question.question_id > question_id).first()
     if question_id == 40:
         return render_template('test-submit.html')
     else:
-        return render_template('exam.html', current_question=questions)
+        return render_template('exam.html', current_question=questions, remaining_time=remaining_seconds)
 
     # if next_question:
     #     return render_template('exam.html', current_question=next_question)
     # else:
     #     return "End of test"  # Or redirect to a different page
+
+@app.route('/update-timer', methods=['POST'])
+def update_timer():
+    data = int(request.form.get('remaining_time'))
+    # remaining_seconds = data.get('remaining_time')
+    # print(f"Remaining time received: {minutes} minutes, {seconds} seconds")
+    # Process the remaining time further if needed
+    return remaining_seconds
 
 # Initialize responses as an empty dictionary
 responses = {}
@@ -105,6 +157,9 @@ responses = {}
 @app.route('/submit-success', methods=['POST'])
 @login_required
 def submit():
+    current_user.is_authenticated = False
+    db.session.commit()
+    # session.pop("user", None)
     return render_template('submission-success.html')
 
 @app.route('/test/<int:question_id>', methods=['GET', 'POST'])
@@ -125,11 +180,98 @@ def test(question_id):
             return "End of the test. Responses collected."
     return render_template('test.html', question=question, answer=responses.get(question_id, ''))
 
+@app.route('/new-exam/<module>', methods=['GET', 'POST'])
+@login_required
+def new_exam(module):
+    if current_user is None:
+        return redirect(url_for('login'))
+    else:
+        questions = Question.query.filter_by(module=module).all()
+        questions_list = [{"question_id": q.question_id 
+                            , "question_header": q.question_header
+                            , "question_query": q.question_query
+                            , "question_img": q.question_img
+                            , "question_img_2": q.question_img_2
+                            , "answer_a": q.answer_a
+                            , "answer_b": q.answer_b
+                            , "answer_c": q.answer_c
+                            , "answer_d": q.answer_d
+                            , "section": q.section
+                            , "module": q.module
+                            , "type": q.question_type
+                            } for q in questions]
+        module = 'verbal-1'
+        time_duration = get_time_duration(module)
+        return render_template('new-exam.html', questions=questions_list, time_duration=time_duration, module=module,
+        username=current_user)
+
+@app.route('/save-answers', methods=['POST'])
+@login_required
+def save_answers():
+    user_answers = request.json
+    username = current_user.username
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    answers_to_save = []
+    for question_id, answer in user_answers.items():
+        answers_to_save.append({'username': username, 'question_id': question_id, 'answer': answer, 'timestamp': timestamp})
+        answer = Answer(username=username, 
+                        question_id=question_id, 
+                        answer=answer, 
+                        timestamp=timestamp,
+                        )
+        db.session.add(answer)
+    print(answers_to_save)
+    db.session.commit()
+
+    
+
+    # Save the answers to the database
+    # database_module.save_answers(answers_to_save)
+
+    # Save the userAnswers to the database
+    # Example: database.save(user_answers)
+    return jsonify({'message': 'Answers saved successfully'}), 200
+
+
+@app.route('/submit-module/<module>')
+def submit_module(module):
+    if module == 'verbal-1':
+        return render_template('verbal1-module-submission.html')
+    elif module == 'verbal-2':
+        return render_template('verbal2-module-submission.html')
+    elif module == 'math-1':
+        return render_template('math1-module-submission.html')
+    else:
+        return render_template('math2-module-submission.html')
+
+
+@app.route('/exam2')
+def exam2():
+
+    questions = Question.query.order_by(Question.question_id).all()
+    remaining_seconds = 32 * 60
+    return render_template('exam2.html', current_question=questions, remaining_time=remaining_seconds)
+
+def get_time_duration(module):
+
+    if 'math' in module:
+
+        return 35 * 60
+    
+    else:
+
+        return 32 * 60
+
 def process_responses(responses):
     for question_id, answer in responses.items():
         # Process each response, e.g., save to a database
         # For demonstration, print the response to the console
         print(f"Question {question_id} - Answer: {answer}")
 
+
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    sess.init_app(app)
     app.run(debug=True)
